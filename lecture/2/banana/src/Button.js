370acda0b1d28d30a03a7bdea18d3b1c6d558d2b
coloringpen <illangayo00@gmail.com>
2023-05-20 05:11:52 +0900

    add style directly on an element through jsx

--- lecture/2/banana/src/Button.js
+++ lecture/2/banana/src/Button.js
@@ -1,9 +1,22 @@
+import PropTypes from "prop-types";
+
 function Button({ text }) {
 	return (
 		<div>
-			<button>{text}</button>
+			<button
+				style={{
+					backgroundColor: "tomato",
+					color: "white",
+				}}
+			>
+				{text}
+			</button>
 		</div>
 	);
 }
+
+Button.propTypes = {
+	text: PropTypes.string.isRequired,
+};
 
 export default Button;
