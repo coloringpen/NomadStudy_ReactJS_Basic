bfb72be9050fe894e293b161c148ce0c9dd2c86f
coloringpen <illangayo00@gmail.com>
2023-05-21 07:49:08 +0900

    First use of useEffect

--- lecture/2/banana/src/routes/apps/First.js
+++ lecture/2/banana/src/routes/apps/First.js
@@ -1,12 +1,21 @@
 import Button from "../../components/start/Button";
 import styled from "../../components/start/First.module.css";
+import { useEffect, useState } from "react";
 
 export default function First() {
+	const [counter, setValue] = useState(0);
+	const onClick = () => setValue((prev) => prev + 1);
+	const iRunOnlyOnce = () => {
+		console.log("I run only once!");
+	};
+	console.log("I run all the time!");
+	useEffect(iRunOnlyOnce, []);
+
 	return (
 		<div>
 			<h1 className={styled.title}>Welcome back!</h1>
-			{/* style속성으로 넣는 게 아니라, className으로 넣는 것임! */}
-			<Button text={"Continue"} />
+			<p>How many times did you click? {counter}</p>
+			<Button onClick={onClick} text={"Click me!"} />
 		</div>
 	);
 }
